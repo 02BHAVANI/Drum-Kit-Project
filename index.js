132cd93851f736486555bf71d19b4db30abe12b2
Bhavani Rajpurohit <91776626+02BHAVANI@users.noreply.github.com>
2023-08-16 00:22:42 +0530

    Drum Kit Project

---
+++ index.js
@@ -0,0 +1,100 @@
+// Event Handlers using button//
+
+for(var i=0; i<document.querySelectorAll(".drum").length; i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",handleClick);
+}
+
+function handleClick()
+{
+    var buttonInnerHTML=this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+    
+}
+
+// Event Handlers using Keyboard Keys //
+
+document.addEventListener("keydown",function(e){
+    
+    makeSound(e.key);
+    buttonAnimation(e.key);
+});
+
+
+function makeSound(key)
+{
+    switch(key)
+    {
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "k":
+            var kick_bass=new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+
+        case "l":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey)
+{
+    var activeButton=document.querySelector("." +currentKey);
+    
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },200);
+
+}
+
+/*
+  How Callbacks Works and Events Work
+
+  function anotherAddEventListener(typeOfEvent,callback){
+    var eventThatHappened={
+        eventType: "keypress",
+        key:"p",
+        durationOfKeypress: 2
+    }
+
+    if(eventThatHappened.eventType===typeOfEvent){
+        callback(eventThatHappened);
+    }
+}
+
+anotherAddEventListener("keypress",function(event){
+    console.log(event);
+});
+
+VM640:2 {eventType: 'keypress', key: 'p', durationOfKeypress: 2}
+
+*/ 
